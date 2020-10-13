1c5b1c776a7d9b7d0dd522159dc01bdbe6325ebf
Jonathan Perry <jpper@umich.edu>
2020-10-13 15:45:49 +0900

    Finished up tutorial and implemented basic war game!

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -9,8 +9,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var leftScoreLabel: UILabel!
     
-    
     @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var playerScore = 0;
+    var CPUScore = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,13 +20,28 @@ class ViewController: UIViewController {
         }
 
     @IBAction func dealTapped(_ sender: Any) {
-        
+        // Randomize some numbers
         let leftNumber = Int.random(in: 2...14);
         let rightNumber = Int.random(in: 2...14);
 
+        // Update the image views
         leftImageView.image = UIImage(named: "card\(leftNumber)");
-        
         rightImageView.image = UIImage(named: "card\(rightNumber)");
+        
+        // Compare the random numbers
+        if leftNumber > rightNumber {
+            // Player wins
+            playerScore += 1;
+            leftScoreLabel.text = String(playerScore);
+        }
+        else if (leftNumber < rightNumber) {
+            // CPU wins
+            CPUScore += 1;
+            rightScoreLabel.text = String(CPUScore);
+        }
+        else {
+            // Tie
+        }
     }
     
 }
